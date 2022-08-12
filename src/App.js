376ba0067cc8156180216b2fea4c59a65d04e58b
heycn <x1chennan@qq.com>
2022-08-12 11:25:04 +0800

    add 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-function App() {
+import React, { useState } from 'react'
+
+export default function App() {
+  const [num, setNum] = useState(0)
+  const myName = 'heycn'
+
   return (
     <div className='App'>
-      <h1>React Basic</h1>
+      <h1>myNum: {myName}</h1>
+      <span>
+        <button onClick={() => setNum(num + 1)}>click me: num + 1</button>
+        num: {num}
+      </span>
     </div>
   )
 }
-
-export default App
