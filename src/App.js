195152e2a9a2561837e057fba0acb7e8241389f6
heycn <x1chennan@qq.com>
2022-08-15 16:39:06 +0800

    preventDefault

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React from 'react'
 import './index.css'
 
 const FunctionComponent = () => {
+  const clickFn = e => {
+    e.preventDefault()
+    console.log(e)
+  }
   return (
-    <h1>this is FunctionComponent</h1>
+    <h1 onClick={clickFn}>
+      <a href='https://baidu.com'>跳转</a>
+    </h1>
   )
 }
 
 class ClassComponent extends React.Component {
   render() {
-    return (
-      <h1>this is ClassComponent</h1>
-    )
+    return <h1>this is ClassComponent</h1>
   }
 }
 
 export default function App() {
-
   return (
     <>
       <FunctionComponent />
