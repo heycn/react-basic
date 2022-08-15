a3c05cee7c3bb531a86f9bd86e4ca89dc0db365a
heycn <x1chennan@qq.com>
2022-08-15 15:36:31 +0800

    组件

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React, { useState } from 'react'
 import './index.css'
 
-export default function App() {
-  const [num, setNum] = useState(0)
-  const myName = 'heycn'
+const Demo = () => {
+  return (
+    <h1>this is demo</h1>
+  )
+}
 
-  const isControlled = true
+export default function App() {
 
   return (
-    <div className='App'>
-      <h1>myNum: {myName}</h1>
-      <span>
-        <button onClick={() => setNum(num + 1)}>click me: num + 1</button>
-        num: {num}
-      </span>
-      <div>
-        我被控制了<span className={isControlled ? 'active' : ''}>111</span>
-      </div>
-    </div>
+    <Demo />
   )
 }
