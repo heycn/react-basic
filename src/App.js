902efadb4ed507d0229799ecd286122e88f70154
heycn <x1chennan@qq.com>
2022-08-16 18:48:23 +0800

    受控组件

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react'
-import './index.css'
+import React, { useState } from 'react'
 
-class Demo extends React.Component {
+class Demo1 extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -9,29 +8,39 @@ class Demo extends React.Component {
     }
   }
 
-  fn = e => {
-    this.setState({
-      message: e.target.value
-    })
-  }
+  fn = e => this.setState({ message: e.target.value })
 
   render() {
     return (
-      <>
-        <input
-          type='text'
-          value={this.state.message}
-          onChange={e => this.fn(e)}
-        />
-      </>
+      <input
+        type='text'
+        value={this.state.message}
+        onChange={e => this.fn(e)}
+      />
     )
   }
+}
+
+const Demo2 = () => {
+  const [message, setMessage] = useState('this is message')
+
+  const changeInput = e => setMessage(e.target.value)
+
+  return (
+    <input
+      type='text'
+      value={message}
+      onChange={e => changeInput(e)}
+    />
+  )
 }
 
 export default function App() {
   return (
     <>
-      <Demo />
+      Demo1: <Demo1 />
+      <br />
+      Demo2: <Demo2 />
     </>
   )
 }
