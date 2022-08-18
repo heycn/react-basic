90ed919ed9f9b1d0f0f47b94e3f412622673a8dc
heycn <x1chennan@qq.com>
2022-08-18 09:58:47 +0800

    组件之间的传递：子传父

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
-import React, { createRef } from 'react'
+import React from 'react'
 
-class Demo1 extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-
-  msgRef = createRef()
-
-  getMsg = () => console.log(this.msgRef.current.value)
-
-  render() {
-    return (
-      <>
-        <input type='text' ref={this.msgRef} />
-        <button onClick={this.getMsg}>get msg</button>
-      </>
-    )
-  }
-}
-
-const Demo2 = () => {
-  const msgRef = createRef()
-  const getMsg = () => console.log(msgRef.current.value)
+const Son = ({ getSonMsg }) => {
+  const msg = 'this is sonMsg'
 
   return (
     <>
-      <input type='text' ref={msgRef} />
-      <button onClick={getMsg}>get msg</button>
+      {msg}
+      <button onClick={() => getSonMsg(msg)}>getSonMsg</button>
     </>
   )
 }
 
 export default function App() {
+  const getSonMsg = sonMsg => console.log(sonMsg)
+
   return (
     <>
-      <Demo1 />
-      <br />
-      <Demo2 />
+      <Son getSonMsg={getSonMsg} />
     </>
   )
 }
