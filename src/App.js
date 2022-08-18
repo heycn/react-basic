e82e2f62898d4f99f3273ae87ce5d73d602e536a
heycn <x1chennan@qq.com>
2022-08-18 15:48:18 +0800

    组件抽离

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
-import React, { createContext } from 'react'
+import React, { useState } from 'react'
 
-const { Provider, Consumer } = createContext()
-
-const Parent = () => {
+const ListItem = ({ item, deleteItem }) => {
   return (
-    <div style={{ border: '1px solid blue', margin: '16px' }}>
-      Parent
-      <Son />
-    </div>
-  )
-}
-
-const Son = () => {
-  return (
-    <div style={{ border: '1px solid red', margin: '16px' }}>
-      Son, this data:
-      <Consumer>{value => value}</Consumer>
+    <div>
+      <h3>{item.name}</h3>
+      <p>{item.price}</p>
+      <p>{item.info}</p>
+      <button onClick={() => deleteItem(item.id)}>删除</button>
     </div>
   )
 }
 
 export default function App() {
-  const data = ` this is App's data`
+  const [list, setList] = useState([
+    {
+      id: 1,
+      name: '超级好吃的棒棒糖',
+      price: 18.8,
+      info: '开业大酬宾，全场8折'
+    },
+    {
+      id: 2,
+      name: '超级好吃的大鸡腿',
+      price: 34.2,
+      info: '开业大酬宾，全场8折'
+    },
+    {
+      id: 3,
+      name: '超级无敌的冰激凌',
+      price: 14.2,
+      info: '开业大酬宾，全场8折'
+    }
+  ])
+
+  const deleteItem = id => {
+    setList(list.filter(item => item.id !== id))
+  }
 
   return (
-    <Provider value={data}>
-      <div style={{ border: '1px solid black' }}>
-        App
-        <Parent />
-      </div>
-    </Provider>
+    <>
+      {list.map(item => (
+        <ListItem key={item.id} item={item} deleteItem={deleteItem} />
+      ))}
+    </>
   )
 }
