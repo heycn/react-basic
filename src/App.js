e1760f90f770d55f982757e0bd80d106cd91de0c
heycn <x1chennan@qq.com>
2022-08-18 16:30:26 +0800

    类型校验

--- src/App.js
+++ src/App.js
@@ -1,47 +1,27 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
-const ListItem = ({ item, deleteItem }) => {
+const Test = ({ list }) => {
   return (
-    <div>
-      <h3>{item.name}</h3>
-      <p>{item.price}</p>
-      <p>{item.info}</p>
-      <button onClick={() => deleteItem(item.id)}>删除</button>
-    </div>
+    <>
+      <div>
+        {list.map(item => (
+          <p key={item}>{item}</p>
+        ))}
+      </div>
+    </>
   )
 }
 
+Test.propTypes = {
+  // 自定义规则
+  list: PropTypes.array
+}
+
 export default function App() {
-  const [list, setList] = useState([
-    {
-      id: 1,
-      name: '超级好吃的棒棒糖',
-      price: 18.8,
-      info: '开业大酬宾，全场8折'
-    },
-    {
-      id: 2,
-      name: '超级好吃的大鸡腿',
-      price: 34.2,
-      info: '开业大酬宾，全场8折'
-    },
-    {
-      id: 3,
-      name: '超级无敌的冰激凌',
-      price: 14.2,
-      info: '开业大酬宾，全场8折'
-    }
-  ])
-
-  const deleteItem = id => {
-    setList(list.filter(item => item.id !== id))
-  }
-
   return (
     <>
-      {list.map(item => (
-        <ListItem key={item.id} item={item} deleteItem={deleteItem} />
-      ))}
+      <Test list={[1, 2, 3]} />
     </>
   )
 }
