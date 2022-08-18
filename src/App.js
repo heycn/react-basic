ddeb82ae87a710a45e967c363cb5943f92025e2b
heycn <x1chennan@qq.com>
2022-08-18 10:28:42 +0800

    子传父

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Son = ({ getSonMsg }) => {
   const msg = 'this is sonMsg'
 
+  const clickHandler = () => getSonMsg(msg)
+
   return (
     <>
       {msg}
-      <button onClick={() => getSonMsg(msg)}>getSonMsg</button>
+      <button onClick={clickHandler}>getSonMsg</button>
     </>
   )
 }
 
 export default function App() {
-  const getSonMsg = sonMsg => console.log(sonMsg)
+  const [msg, setMsg] = useState(void 0)
+  const getSonMsg = sonMsg => setMsg(sonMsg)
 
   return (
     <>
       <Son getSonMsg={getSonMsg} />
+      <h1>{msg}</h1>
     </>
   )
 }
