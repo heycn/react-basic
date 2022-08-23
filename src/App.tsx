27fc802af39abf0c9365a89313a4f340695f217d
heycn <x1chennan@qq.com>
2022-08-23 11:14:11 +0800

    fixbug

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import useLocalStorage from './hooks/useLocalStorage'
 
 function App() {
-  const { message, setMessage } = useLocalStorage('myKey', 'myValue1')
+  const [message, setMessage] = useLocalStorage('myKey', 'myValue1')
 
   setTimeout(() => {
-    setMessage('myValue2')
+    setMessage('myValue2') //FIXME: "string | Dispatch<SetStateAction<string>>" 类型的部分要素不可调用。
   }, 2000)
 
   return (
